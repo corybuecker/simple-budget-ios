94ea962addc7deec90148e9feba0bc2ccaaf124e
Cory Buecker <email@corybuecker.com>
2026-03-22 13:21:05 -0500

    --wip-- [skip ci]

--- SimpleBudget/ContentView.swift
+++ SimpleBudget/ContentView.swift
@@ -3,7 +3,6 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            
             Tab("Accounts", systemImage: "dollarsign") {
                 Text("test")
             }
