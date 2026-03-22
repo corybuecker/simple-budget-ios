236705bd4e18dc9bee02b5781092f6db20ccc6a1
Cory Buecker <email@corybuecker.com>
2026-03-22 11:53:59 -0500

    feat: initial commit

---
+++ SimpleBudget/ContentView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Tab("Accounts", systemImage: "dollarsign") {
+                Text("test")
+            }
+            Tab("Envelopes", systemImage: "dollarsign") {
+                Text("test")
+            }
+            Tab("Goals", systemImage: "dollarsign") {
+                Text("test")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleBudget/SimpleBudgetApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct SimpleBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
